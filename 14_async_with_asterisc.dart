c41d40caa9fb8ba5f6eed732898330439a66c86d
Eric Denis Laura <ericdenislaura@gmail.com>
2023-04-27 03:40:09 -0300

    async asterisc

---
+++ 14_async_with_asterisc.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  emitNumber().listen((int value) {
+    print('Stream value: $value');
+  });
+}
+
+Stream<int> emitNumber() async* {
+  final valuesToEmit = [1, 2, 3, 4, 5];
+  for (int i in valuesToEmit) {
+    await Future.delayed(const Duration(seconds: 1));
+    yield i;
+  }
+}
