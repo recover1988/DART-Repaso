9b47832b47b152b00b1464dff87192718a6d487b
Eric Denis Laura <ericdenislaura@gmail.com>
2023-04-27 01:35:15 -0300

    extends

--- 08_class-abstractas_enum.dart
+++ 08_class-abstractas_enum.dart
@@ -1,5 +1,15 @@
 void main(List<String> args) {
   // final windPlant = EnergyPlant();
+  final windPlant = WindPlant(initialEnergy: 100);
+  print(windPlant);
+  print('wind: ${chargePhone(windPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enoguh energy');
+  }
+  return plant.energyLeft - 10;
 }
 
 // * enum para indicar elementos que no van a cambiar mucho
@@ -12,4 +22,16 @@ abstract class EnergyPlant {
   EnergyPlant({required this.energyLeft, required this.type});
 
   void consumeEnergy(double amount);
+}
+
+// extends o implements
+// * extends es agregar o implementar de otra clase
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlanType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
 }
